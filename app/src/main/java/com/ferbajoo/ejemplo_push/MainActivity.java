6ec017f9db9c12d3bf203922e842f831bca83136
Ferbajoo <fernando@indatcom.mx>
2016-05-04 17:27:56 -0500

    Pruebas con boton

--- app/src/main/java/com/ferbajoo/ejemplo_push/MainActivity.java
+++ app/src/main/java/com/ferbajoo/ejemplo_push/MainActivity.java
@@ -2,15 +2,22 @@ package com.ferbajoo.ejemplo_push;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.urbanairship.UAirship;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button mensaje;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mensaje = (Button)findViewById(R.id.btn1);
 
         UAirship.takeOff(getApplication(), new UAirship.OnReadyCallback() {
             @Override
@@ -23,6 +30,12 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+        mensaje.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Fernando Uribe", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 
